0a1369a0cbe7790f306eae6259d1ed97e9d84767
ImplementLife <lmplementlifeg@gmail.com>
2023-05-21 23:09:14 +0300

    Refactoring main menu

--- app/src/main/java/com/impllife/split/ui/fragment/MainMenuFragment.java
+++ app/src/main/java/com/impllife/split/ui/fragment/MainMenuFragment.java
@@ -1,32 +1,33 @@
 package com.impllife.split.ui.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.GridLayout;
+import androidx.navigation.NavController;
 import com.impllife.split.R;
-import com.impllife.split.ui.MainActivity;
-import com.impllife.split.ui.view.BtnMainMenu;
+import com.impllife.split.ui.custom.component.NavFragment;
+import com.impllife.split.ui.custom.component.BaseView;
+
+import java.util.function.Consumer;
 
 import static android.view.Gravity.FILL_HORIZONTAL;
 
 public class MainMenuFragment extends NavFragment {
     private GridLayout grid;
 
+    public MainMenuFragment() {
+        super(R.layout.fragment_main_menu);
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = createView(R.layout.fragment_main_menu, inflater, container);
-        MainActivity.getInstance().hideHead();
-
-        grid = view.findViewById(R.id.grid);
-
+    protected void init() {
+        grid = findViewById(R.id.grid);
         for (MainMenuButtonDefinition btn : MainMenuButtonDefinition.values()) {
-            BtnMainMenu btnView = new BtnMainMenu(btn, inflater, grid);
-            btnView.setOnClickListener(v -> btn.getAction().accept(navController));
-            setBtn(btnView.getRoot(), btn.getDefaultRow(), btn.getDefaultCol());
+            BaseView view = new BaseView(R.layout.view_btn_main_menu, grid);
+            view.setOnClickListener(v -> btn.getAction().accept(navController));
+            view.setTextViewById(R.id.tv_name, btn.getName());
+            view.setImgResById(R.id.img, btn.getImgId());
+            setBtn(view.getRoot(), btn.getDefaultRow(), btn.getDefaultCol());
         }
-        return view;
     }
 
     private void setBtn(View view, int row, int col) {
@@ -37,5 +38,54 @@ public class MainMenuFragment extends NavFragment {
         layoutParams.height = 100;
 
         grid.addView(view, layoutParams);
+    }
+
+    public enum MainMenuButtonDefinition {
+        BTN_CONTACTS("Contacts", 0, 0, R.drawable.ic_svg_group, n -> n.navigate(R.id.fragment_contacts)),
+        BTN_QR("My link qr", 0, 1, R.drawable.ic_svg_cancel, n -> {}),
+        BTN_QR_SCANNER("QR Scan", 0, 2, R.drawable.ic_svg_cancel, n -> {}),
+
+        BTN_SETTINGS("Settings", 1, 0, R.drawable.ic_svg_cancel, n -> {}),
+        BTN_NOTIFICATIONS("Notify's", 1, 1, R.drawable.ic_svg_notifications, n -> n.navigate(R.id.fragment_notification)),
+        BTN_DISCOUNTS("Discounts", 1, 2, R.drawable.ic_svg_cancel, n -> {}),
+
+        BTN_NEW_REQUISITION("New Req", 2, 2, R.drawable.ic_svg_cancel, n -> {}),
+        BTN_BUDGET("Budget", 3, 0, R.drawable.ic_svg_money, n -> n.navigate(R.id.fragment_budget_menu)),
+        BTN_NEW_TRANSACTION("New Trn", 2, 1, R.drawable.ic_svg_receipt, n -> n.navigate(R.id.fragment_transaction_setup)),
+
+        BTN_NFC_PAY("NFC Pay", 3, 1, R.drawable.ic_svg_cancel, n -> {}),
+        BTN_HISTORY("History", 2, 0, R.drawable.ic_svg_history, n -> n.navigate(R.id.fragment_history)),
+
+        ;
+
+        MainMenuButtonDefinition(String name, int defaultRow, int defaultCol, int imgId, Consumer<NavController> action) {
+            this.name = name;
+            this.imgId = imgId;
+            this.action = action;
+            this.defaultCol = defaultCol;
+            this.defaultRow = defaultRow;
+        }
+
+        private final int defaultCol;
+        private final int defaultRow;
+        private final String name;
+        private final int imgId;
+        private final Consumer<NavController> action;
+
+        public int getDefaultCol() {
+            return defaultCol;
+        }
+        public int getDefaultRow() {
+            return defaultRow;
+        }
+        public String getName() {
+            return name;
+        }
+        public Consumer<NavController> getAction() {
+            return action;
+        }
+        public int getImgId() {
+            return imgId;
+        }
     }
 }
