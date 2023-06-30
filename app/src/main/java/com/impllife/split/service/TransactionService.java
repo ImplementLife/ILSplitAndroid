78a5e2d865627332097edeb2f5b4b39547dfd405
ImplementLife <lmplementlifeg@gmail.com>
2023-06-30 22:37:40 +0300

    [UI] Transaction Setup -> fix update bug

--- app/src/main/java/com/impllife/split/service/TransactionService.java
+++ app/src/main/java/com/impllife/split/service/TransactionService.java
@@ -50,39 +50,35 @@ public class TransactionService implements TransactionDao {
     }
 
     private void processTrn(Transaction trn) {
-        List<Account> affectedAccounts = new ArrayList<>();
+        rollbackTransaction(trn);
 
+        BigDecimal sum = new BigDecimal(trn.getSum());
+        Account fromAccount = accountDao.findById(trn.getFromAccountId());
+        if (fromAccount != null) {
+            fromAccount.setAmount(BigDecimal.valueOf(fromAccount.getAmount()).subtract(sum).doubleValue());
+            accountDao.update(fromAccount);
+        }
+        Account toAccount = accountDao.findById(trn.getToAccountId());
+        if (toAccount != null) {
+            toAccount.setAmount(BigDecimal.valueOf(toAccount.getAmount()).add(sum).doubleValue());
+            accountDao.update(toAccount);
+        }
+    }
+
+    private void rollbackTransaction(Transaction trn) {
         Transaction oldTrn = trnDao.findById(trn.getId() != null ? trn.getId() : -1);
         if (oldTrn != null) {
             BigDecimal sum = new BigDecimal(oldTrn.getSum());
             Account fromAccount = accountDao.findById(oldTrn.getFromAccountId());
             if (fromAccount != null) {
                 fromAccount.setAmount(BigDecimal.valueOf(fromAccount.getAmount()).add(sum).doubleValue());
-                affectedAccounts.add(fromAccount);
+                accountDao.update(fromAccount);
             }
             Account toAccount = accountDao.findById(oldTrn.getToAccountId());
             if (toAccount != null) {
                 toAccount.setAmount(BigDecimal.valueOf(toAccount.getAmount()).subtract(sum).doubleValue());
-                affectedAccounts.add(toAccount);
+                accountDao.update(toAccount);
             }
-        }
-
-
-        BigDecimal sum = new BigDecimal(trn.getSum());
-        Account fromAccount = accountDao.findById(trn.getFromAccountId());
-        if (fromAccount != null) {
-            fromAccount.setAmount(BigDecimal.valueOf(fromAccount.getAmount()).subtract(sum).doubleValue());
-            affectedAccounts.add(fromAccount);
-        }
-        Account toAccount = accountDao.findById(trn.getToAccountId());
-        if (toAccount != null) {
-            toAccount.setAmount(BigDecimal.valueOf(toAccount.getAmount()).add(sum).doubleValue());
-            affectedAccounts.add(toAccount);
-        }
-
-
-        for (Account account : affectedAccounts) {
-            accountDao.update(account);
         }
     }
 
@@ -103,6 +99,7 @@ public class TransactionService implements TransactionDao {
 
     @Override
     public void delete(Transaction transaction) {
+        rollbackTransaction(transaction);
         trnDao.delete(transaction);
     }
 
