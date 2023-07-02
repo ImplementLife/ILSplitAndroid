40e3d668284900069fb40912878bbe91d338d501
ImplementLife <lmplementlifeg@gmail.com>
2023-07-03 01:39:55 +0300

    refactor main menu (draft p2)

--- app/src/main/java/com/impllife/split/ui/fragment/MainMenuFragment.java
+++ app/src/main/java/com/impllife/split/ui/fragment/MainMenuFragment.java
@@ -1,9 +1,9 @@
 package com.impllife.split.ui.fragment;
 
-import android.view.View;
-import android.widget.GridLayout;
+import android.widget.GridView;
 import androidx.navigation.NavController;
 import com.impllife.split.R;
+import com.impllife.split.ui.custom.adapter.GridListAdapter;
 import com.impllife.split.ui.custom.component.BaseView;
 import com.impllife.split.ui.custom.component.NavFragment;
 import com.impllife.split.ui.dialog.ChooseQrAction;
@@ -11,10 +11,8 @@ import com.impllife.split.ui.dialog.ChooseTrnTemplateDialog;
 
 import java.util.function.Consumer;
 
-import static android.view.Gravity.FILL_HORIZONTAL;
-
 public class MainMenuFragment extends NavFragment {
-    private GridLayout grid;
+    private GridView grid;
 
     public MainMenuFragment() {
         super(R.layout.fragment_main_menu);
@@ -23,42 +21,31 @@ public class MainMenuFragment extends NavFragment {
     @Override
     protected void init() {
         grid = findViewById(R.id.grid);
-        for (MainMenuButtonDefinition btn : MainMenuButtonDefinition.values()) {
+        GridListAdapter<MainMenuButtonDefinition> adapter = new GridListAdapter<>(MainMenuButtonDefinition.values(), (data, parent) -> {
             BaseView view = new BaseView(R.layout.view_btn_main_menu, grid);
-            view.setOnClickListener(v -> btn.getAction().accept(navController));
-            view.setTextViewById(R.id.tv_name, btn.getName());
-            view.setImgResById(R.id.img, btn.getImgId());
-            setBtn(view.getRoot(), btn.getDefaultRow(), btn.getDefaultCol());
-        }
-    }
 
-    private void setBtn(View view, int row, int col) {
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(
-            GridLayout.spec(row, 1f),
-            GridLayout.spec(col, 1f));
-        layoutParams.setGravity(FILL_HORIZONTAL);
-        layoutParams.height = 100;
-
-        grid.addView(view, layoutParams);
+            view.setOnClickListener(v -> data.getAction().accept(navController));
+            view.setTextViewById(R.id.tv_name, data.getName());
+            view.setImgResById(R.id.img, data.getImgId());
+            return view.getRoot();
+        });
+        grid.setAdapter(adapter);
     }
 
     public enum MainMenuButtonDefinition {
+        BTN_SETTINGS("Settings", 0, 0, R.drawable.ic_svg_cancel, n -> {}),
         BTN_CONTACTS("Contacts", 0, 1, R.drawable.ic_svg_group, n -> n.navigate(R.id.fragment_contact)),
         BTN_QR_SCANNER("QR", 0, 2, R.drawable.ic_svg_qr_code, n -> {new ChooseQrAction(n).show();}),
-
-        BTN_SETTINGS("Settings", 0, 0, R.drawable.ic_svg_cancel, n -> {}),
+        BTN_HISTORY("History", 1, 0, R.drawable.ic_svg_history, n -> n.navigate(R.id.fragment_history)),
         BTN_NOTIFICATIONS("Notify's", 1, 1, R.drawable.ic_svg_notifications, n -> n.navigate(R.id.fragment_notify_list)),
 //        BTN_DISCOUNTS("Discounts", 1, 2, R.drawable.ic_svg_cancel, n -> {}),
-
         BTN_NEW_SPLIT("New Split", 1, 2, R.drawable.ic_svg_groups, n -> n.navigate(R.id.fragment_requisition_setup)),
         BTN_BUDGET("Budget", 2, 0, R.drawable.ic_svg_money, n -> n.navigate(R.id.fragment_budget_menu)),
-        BTN_NEW_TRANSACTION("New Trn", 2, 2, R.drawable.ic_svg_receipt, n -> n.navigate(R.id.fragment_transaction_setup)),
-        BTN_NEW_TRANSACTION_BY_TEMPLATE("New Trn by\ntemplate", 2, 1, R.drawable.ic_svg_receipt_long, n -> {
+        BTN_NEW_TRANSACTION_BY_TEMPLATE("Tmpl Trn", 2, 1, R.drawable.ic_svg_receipt_long, n -> {
             new ChooseTrnTemplateDialog().show();
         }),
-
+        BTN_NEW_TRANSACTION("New Trn", 2, 2, R.drawable.ic_svg_receipt, n -> n.navigate(R.id.fragment_transaction_setup)),
 //        BTN_NFC_PAY("NFC Pay", 3, 1, R.drawable.ic_svg_cancel, n -> {}),
-        BTN_HISTORY("History", 1, 0, R.drawable.ic_svg_history, n -> n.navigate(R.id.fragment_history)),
 
         ;
 
