3c72061fc568a87f950dc9e44419b32d76cbc43c
ImplementLife <lmplementlifeg@gmail.com>
2023-09-09 23:42:14 +0300

    [UI] - draft impl ui budget

---
+++ app/src/main/java/com/impllife/split/ui/fragment/BudgetFragment.java
@@ -0,0 +1,10 @@
+package com.impllife.split.ui.fragment;
+
+import com.impllife.split.R;
+import com.impllife.split.ui.custom.component.NavFragment;
+
+public class BudgetFragment extends NavFragment {
+    public BudgetFragment() {
+        super(R.layout.fragment_budget, "Budget");
+    }
+}
