55b453e049ad57d0eba1937cec79f942fabb7157
ImplementLife <lmplementlifeg@gmail.com>
2023-09-09 15:53:09 +0300

    [UI] - impl view percent

--- app/src/main/java/com/impllife/split/ui/view/TransactionListItemDate.java
+++ app/src/main/java/com/impllife/split/ui/view/TransactionListItemDate.java
@@ -6,16 +6,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import com.impllife.split.R;
 import com.impllife.split.ui.custom.component.BaseView;
+import com.impllife.split.ui.custom.component.StatusBar;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TransactionListItemDate extends BaseView {
     @SuppressLint("SimpleDateFormat")
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-
+    private static final BigDecimal budget = BigDecimal.valueOf(900);
     private TextView tvDate;
     private TextView tvSumTotal;
+    private TextView tvPercent;
+    private StatusBar statusBar;
 
     public TransactionListItemDate(LayoutInflater inflater, ViewGroup rootForThis, Date date) {
         super(inflater, R.layout.view_transactoin_list_item_date, rootForThis);
@@ -26,6 +31,8 @@ public class TransactionListItemDate extends BaseView {
     private void init() {
         tvDate = findViewById(R.id.tv_date);
         tvSumTotal = findViewById(R.id.tv_sum_total);
+        tvPercent = findViewById(R.id.tv_percent);
+        statusBar = findViewById(R.id.status_bar);
     }
 
     public void setData(Date date) {
@@ -34,5 +41,10 @@ public class TransactionListItemDate extends BaseView {
 
     public void setData(String sumTotal) {
         tvSumTotal.setText(sumTotal);
+        BigDecimal sum = new BigDecimal(sumTotal);
+        if (sum.compareTo(BigDecimal.ZERO) < 0) sum = sum.negate();
+        BigDecimal asPercent = sum.multiply(BigDecimal.valueOf(100)).divide(budget, RoundingMode.CEILING);
+        statusBar.setProgress(asPercent.intValue());
+        tvPercent.setText(asPercent.intValue() + "%");
     }
 }
