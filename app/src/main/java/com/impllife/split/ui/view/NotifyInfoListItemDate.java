95b0ef4bd393a6aae6bf2138f11c95e6d342a738
ImplementLife <lmplementlifeg@gmail.com>
2023-09-27 01:07:31 +0300

    [UI] draft add 'Add all' btn

--- app/src/main/java/com/impllife/split/ui/view/NotifyInfoListItemDate.java
+++ app/src/main/java/com/impllife/split/ui/view/NotifyInfoListItemDate.java
@@ -1,17 +1,20 @@
 package com.impllife.split.ui.view;
 
 import com.impllife.split.R;
+import com.impllife.split.data.jpa.entity.NotificationInfo;
 import com.impllife.split.service.util.Formatters;
 import com.impllife.split.ui.custom.adapter.AltRecyclerViewListAdapter;
 import com.impllife.split.ui.custom.component.BaseView;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.impllife.split.service.util.Util.isToday;
 import static com.impllife.split.service.util.Util.isYesterday;
 
 public class NotifyInfoListItemDate extends AltRecyclerViewListAdapter.Data<Date> {
     private Date date;
+    private List<NotificationInfo> notifys;
 
     public NotifyInfoListItemDate(Date date) {
         super(R.layout.view_notify_list_item_date);
@@ -20,10 +23,13 @@ public class NotifyInfoListItemDate extends AltRecyclerViewListAdapter.Data<Date
 
     @Override
     public void bindData(BaseView view) {
+        view.findViewById(R.id.btn_add_all).setOnClickListener(v -> {
+
+        });
         if (isToday(date)) {
-            view.setTextViewById(R.id.tv_date, "Today");
+            view.setTextViewById(R.id.tv_date, Formatters.formatDDMM(date) + " Today");
         } else if (isYesterday(date)) {
-            view.setTextViewById(R.id.tv_date, "Yesterday");
+            view.setTextViewById(R.id.tv_date, Formatters.formatDDMM(date) + " Yesterday");
         } else {
             view.setTextViewById(R.id.tv_date, Formatters.formatDDMM(date));
         }
---
+++ app/src/main/java/com/impllife/split/ui/view/list/QuickAddNotify.java
@@ -0,0 +1,11 @@
+package com.impllife.split.ui.view.list;
+
+import android.view.ViewGroup;
+import com.impllife.split.R;
+import com.impllife.split.ui.custom.component.BaseView;
+
+public class QuickAddNotify extends BaseView {
+    public QuickAddNotify(ViewGroup rootForThis) {
+        super(R.layout.view_list_item_quick_add_notify, rootForThis);
+    }
+}
