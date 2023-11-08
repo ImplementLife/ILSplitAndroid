4e790a5c17b5c241dae355cf1d123a9db7c9854c
ImplementLife <lmplementlifeg@gmail.com>
2023-11-08 02:12:23 +0200

    refactor Contacts fragment

--- app/src/main/java/com/impllife/split/ui/fragment/ContactFragment.java
+++ app/src/main/java/com/impllife/split/ui/fragment/ContactFragment.java
@@ -4,17 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.impllife.split.R;
+import com.impllife.split.ui.custom.adapter.TabPagerAdapter;
 import com.impllife.split.ui.custom.component.NavFragment;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactFragment extends NavFragment {
 
@@ -23,37 +21,19 @@ public class ContactFragment extends NavFragment {
         View view = inflater.inflate(R.layout.fragment_contact, container, false);
         setNavTitle("Contacts");
 
-        Map<Integer, TabInfo> tabInfoMap = new TreeMap<>();
-        tabInfoMap.put(0, new TabInfo("Peoples", new ContactPeoplesFragment()));
-        tabInfoMap.put(1, new TabInfo("Groups", new ContactGroupListFragment()));
-
         ViewPager2 pager = view.findViewById(R.id.pager);
-        FragmentStateAdapter pageAdapter = new FragmentStateAdapter(this) {
-            public int getItemCount() {
-                return tabInfoMap.size();
-            }
-            public Fragment createFragment(int position) {
-                return Objects.requireNonNull(tabInfoMap.get(position)).fragment;
-            }
-        };
-        pager.setAdapter(pageAdapter);
-
         TabLayout tabLayout = view.findViewById(R.id.tab);
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, pager, (tab, position) -> {
-            tab.setText(Objects.requireNonNull(tabInfoMap.get(position)).name);
-        });
-        tabLayoutMediator.attach();
+
+        List<TabPagerAdapter.TabInfo> tabInfoList = new ArrayList<>();
+        tabInfoList.add(new TabPagerAdapter.TabInfo("Peoples", new ContactPeoplesFragment()));
+        tabInfoList.add(new TabPagerAdapter.TabInfo("Groups", new ContactGroupListFragment()));
+
+        pager.setAdapter(new TabPagerAdapter(this, tabInfoList));
+
+        new TabLayoutMediator(tabLayout, pager, (tab, position) -> {
+            tab.setText(tabInfoList.get(position).getName());
+        }).attach();
 
         return view;
-    }
-
-    private static class TabInfo {
-        String name;
-        Fragment fragment;
-
-        public TabInfo(String name, Fragment fragment) {
-            this.name = name;
-            this.fragment = fragment;
-        }
     }
 }
